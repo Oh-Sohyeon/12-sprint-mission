7e52ed93457a21a4d19d261af28e0eca82218a92
Oh-Sohyeon <ohsobbong@kyonggi.ac.kr>
2026-04-19 15:27:36 +0900

    feat: add smoke test

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,42 +1,104 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.ChannelType;
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.dto.auth.AuthResponse;
+import com.sprint.mission.discodeit.dto.auth.LoginRequest;
+import com.sprint.mission.discodeit.dto.channel.ChannelResponse;
+import com.sprint.mission.discodeit.dto.channel.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
+import com.sprint.mission.discodeit.dto.message.MessageResponse;
+import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.user.UserResponse;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.AuthService;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 
-    static User setupUser(UserService userService) {
-        return userService.create("woody");
-    }
-
-    static Channel setupChannel(ChannelService channelService) {
-        return channelService.create(ChannelType.PUBLIC, "공지", "공지 채널입니다.");
-    }
-
-   static void messageCreateTest(MessageService messageService, Channel channel, User author) {
-        Message message = messageService.create("안녕하세요.");
-        System.out.println("메시지 생성: " + message.getId());
-   }
-
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+        ConfigurableApplicationContext context =
+                SpringApplication.run(DiscodeitApplication.class, args);
+
+        runSmokeTest(context);
+    }
+
+    private static void runSmokeTest(ConfigurableApplicationContext context) {
+        Environment env = context.getEnvironment();
+
+        String repoType = env.getProperty("discodeit.repository.type", "jcf");
+        String fileDirectory = env.getProperty("discodeit.repository.file-directory", ".discodeit");
+
+        UserRepository userRepository = context.getBean(UserRepository.class);
+        ChannelRepository channelRepository = context.getBean(ChannelRepository.class);
+        MessageRepository messageRepository = context.getBean(MessageRepository.class);
 
         UserService userService = context.getBean(UserService.class);
         ChannelService channelService = context.getBean(ChannelService.class);
         MessageService messageService = context.getBean(MessageService.class);
+        AuthService authService = context.getBean(AuthService.class);
 
-        User user = setupUser(userService);
-        Channel channel = setupChannel(channelService);
-        messageCreateTest(messageService,channel, user);
+        long suffix = System.currentTimeMillis();
+        String username = "woody" + suffix;
+        String email = "woody" + suffix + "@codeit.com";
+        String password = "woody1234";
+
+        System.out.println("========== SMOKE TEST START ==========");
+        System.out.println("[CONFIG] repository.type = " + repoType);
+        System.out.println("[CONFIG] repository.file-directory = " + fileDirectory);
+
+        System.out.println("[BEAN] UserRepository = " + userRepository.getClass().getSimpleName());
+        System.out.println("[BEAN] ChannelRepository = " + channelRepository.getClass().getSimpleName());
+        System.out.println("[BEAN] MessageRepository = " + messageRepository.getClass().getSimpleName());
+
+        UserResponse user = userService.create(
+                new UserCreateRequest(
+                        username,
+                        email,
+                        password,
+                        null
+                )
+        );
+        System.out.println("[CREATE] userId = " + user.id());
+
+        ChannelResponse channel = channelService.createPublic(
+                new PublicChannelCreateRequest(
+                        "공지-" + suffix,
+                        "스모크 테스트용 공개 채널"
+                )
+        );
+        System.out.println("[CREATE] channelId = " + channel.id());
+
+        MessageResponse message = messageService.create(
+                new MessageCreateRequest(
+                        "안녕하세요. 스모크 테스트 메시지입니다.",
+                        channel.id(),
+                        user.id(),
+                        List.of()
+                )
+        );
+        System.out.println("[CREATE] messageId = " + message.id());
+
+        AuthResponse loginResult = authService.login(
+                new LoginRequest(username, password)
+        );
+        System.out.println("[LOGIN] success userId = " + loginResult.id());
+
+        List<ChannelResponse> channels = channelService.findAllByUserId(user.id());
+        List<MessageResponse> messages = messageService.findAllByChannelId(channel.id());
+
+        System.out.println("[QUERY] visible channel count = " + channels.size());
+        System.out.println("[QUERY] message count in channel = " + messages.size());
+
+        System.out.println("=========== SMOKE TEST END ===========");
     }
-
 }
